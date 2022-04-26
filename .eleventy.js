3f67ced0f9c3c6d0efcbf871d319aef158354dc5
RB <76770572+Rendangbike@users.noreply.github.com>
2022-04-26 13:15:05 +0700

    working

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+const {DateTime} = require("luxon")
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("./src/style.css");
+    eleventyConfig.addPassthroughCopy("./src/assets");
+    
+    return {
+        dir: {
+            input: "src",
+            output: "public"
+        }
+    };
+}
