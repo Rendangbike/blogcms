465daceb5f4134bffd5fb0eaade271ce015c8277
RB <76770572+Rendangbike@users.noreply.github.com>
2022-04-26 17:49:10 +0700

    Update UI

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,3 @@
-const {DateTime} = require("luxon")
-
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
