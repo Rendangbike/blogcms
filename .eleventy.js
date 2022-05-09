022566523734a32047e399769f2f99cf1583d235
RB <76770572+Rendangbike@users.noreply.github.com>
2022-05-09 12:42:55 +0700

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/admin");
+    eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
     
     return {
         dir: {
