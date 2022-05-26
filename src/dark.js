30fada32f02d8a0b4ed5aa46b3f3654a0420f221
RB <76770572+Rendangbike@users.noreply.github.com>
2022-05-26 15:55:02 +0700

    h1 fix

--- src/dark.js
+++
@@ -1,7 +0,0 @@
-// function color_change() {
-    if ( localStorage.getItem("theme") === null) {
-        let theme = "light";
-    } else {
-        let theme = localStorage.getItem("theme");
-    }
-// }
