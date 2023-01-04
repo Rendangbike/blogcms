ce94e1833be402a6c64f026f212ecaab92fcb0b4
RB <76770572+Rendangbike@users.noreply.github.com>
2023-01-04 21:00:48 +0700

    h

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/style.css");
     eleventyConfig.addPassthroughCopy("./src/assets");
     eleventyConfig.addPassthroughCopy("./src/admin");
-    eleventyConfig.addPassthroughCopy("./src/dark.js");
 
     
     return {
